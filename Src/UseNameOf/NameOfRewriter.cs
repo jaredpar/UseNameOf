61a1508cb4b6f679211a2dcc3f88bef7a21a325d
Jared Parsons <jaredpar@microsoft.com>
2015-01-01 10:58:45 -0500

    Restrict to actual ArgumentException types

--- Src/UseNameOf/NameOfRewriter.cs
+++ Src/UseNameOf/NameOfRewriter.cs
@@ -12,11 +12,13 @@ namespace UseNameOf
     internal sealed class NameOfRewriter : CSharpSyntaxRewriter
     {
         private readonly SemanticModel _semanticModel;
+        private readonly ITypeSymbol _argumentExceptionType;
         private readonly Dictionary<LiteralExpressionSyntax, NameOfExpressionSyntax> _map = new Dictionary<LiteralExpressionSyntax, NameOfExpressionSyntax>();
 
         internal NameOfRewriter(SemanticModel semanticModel)
         {
             _semanticModel = semanticModel;
+            _argumentExceptionType = semanticModel.Compilation.GetTypeByMetadataName("System.ArgumentException");
         }
 
         public override SyntaxNode VisitLiteralExpression(LiteralExpressionSyntax node)
@@ -30,28 +32,47 @@ namespace UseNameOf
             return base.VisitLiteralExpression(node);
         }
 
+        /// <summary>
+        /// Rewrite any creation of an <see cref="ArgumentException" /> which uses a string literal
+        /// that maps to a parameter name to use nameof for that parameter instead. 
+        /// </summary>
         public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
         {
-            var arguments = node.ArgumentList.Arguments;
-            if (arguments.Count > 0)
+            if (!IsArgumentExceptionType(node.Type))
             {
-                var expr = arguments[0].Expression;
-                if (expr != null && expr.CSharpKind() == SyntaxKind.StringLiteralExpression)
-                {
-                    var stringExpr = (LiteralExpressionSyntax)expr;
-                    var position = stringExpr.GetLocation().SourceSpan.Start;
-                    var identExpr = SyntaxFactory.IdentifierName(stringExpr.Token.ValueText);
-                    var symbolInfo = _semanticModel.GetSpeculativeSymbolInfo(position, identExpr, SpeculativeBindingOption.BindAsExpression);
-                    if (symbolInfo.Symbol != null && symbolInfo.Symbol.Kind == SymbolKind.Parameter)
-                    {
-                        _map[stringExpr] = SyntaxFactory.NameOfExpression(
-                            SyntaxFactory.IdentifierName("nameof"),
-                            identExpr);
-                    }
-                }
+                return base.VisitObjectCreationExpression(node);
+            }
+
+            var arguments = node.ArgumentList.Arguments;
+            if (arguments.Count == 0)
+            {
+                return base.VisitObjectCreationExpression(node);
+            }
+
+            var expr = arguments[0].Expression;
+            if (expr == null || expr.CSharpKind() != SyntaxKind.StringLiteralExpression)
+            {
+                return base.VisitObjectCreationExpression(node);
+            }
+
+            var stringExpr = (LiteralExpressionSyntax)expr;
+            var position = stringExpr.GetLocation().SourceSpan.Start;
+            var identExpr = SyntaxFactory.IdentifierName(stringExpr.Token.ValueText);
+            var symbolInfo = _semanticModel.GetSpeculativeSymbolInfo(position, identExpr, SpeculativeBindingOption.BindAsExpression);
+            if (symbolInfo.Symbol != null && symbolInfo.Symbol.Kind == SymbolKind.Parameter)
+            {
+                _map[stringExpr] = SyntaxFactory.NameOfExpression(
+                    SyntaxFactory.IdentifierName("nameof"),
+                    identExpr);
             }
 
             return base.VisitObjectCreationExpression(node);
+        }
+
+        private bool IsArgumentExceptionType(TypeSyntax typeSyntax)
+        {
+            var conversion = _semanticModel.ClassifyConversion(typeSyntax, _argumentExceptionType);
+            return conversion.IsReference && conversion.IsImplicit;
         }
     }
 }
