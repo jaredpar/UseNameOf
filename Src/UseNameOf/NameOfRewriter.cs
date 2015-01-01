0c8320a337b800cd6e6e3e4a67106dff380077c9
Jared Parsons <jaredpar@microsoft.com>
2015-01-01 10:33:59 -0500

    Basics working

---
+++ Src/UseNameOf/NameOfRewriter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseNameOf
+{
+    public sealed class NameOfRewriter : CSharpSyntaxRewriter
+    {
+        private readonly Dictionary<LiteralExpressionSyntax, NameOfExpressionSyntax> _map = new Dictionary<LiteralExpressionSyntax, NameOfExpressionSyntax>();
+
+        public override SyntaxNode VisitLiteralExpression(LiteralExpressionSyntax node)
+        {
+            NameOfExpressionSyntax nameOfExpr;
+            if (_map.TryGetValue(node, out nameOfExpr))
+            {
+                return nameOfExpr;
+            }
+
+            return base.VisitLiteralExpression(node);
+        }
+
+        public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            var arguments = node.ArgumentList.Arguments;
+            if (arguments.Count > 0)
+            {
+                var expr = arguments[0].Expression;
+                if (expr != null && expr.CSharpKind() == SyntaxKind.StringLiteralExpression)
+                {
+                    var stringExpr = (LiteralExpressionSyntax)expr;
+                    _map[stringExpr] = SyntaxFactory.NameOfExpression(
+                        SyntaxFactory.IdentifierName("nameof"),
+                        SyntaxFactory.IdentifierName(stringExpr.Token.ValueText));
+                }
+            }
+
+            return base.VisitObjectCreationExpression(node);
+        }
+    }
+}
--- Src/UseNameOf/Program.cs
+++ Src/UseNameOf/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UseNameOf
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var text = File.ReadAllText(args[0]);
+            var tree = CSharpSyntaxTree.ParseText(text);
+            var node = (new NameOfRewriter()).Visit(tree.GetRoot());
+            Console.WriteLine(node.ToFullString());
         }
     }
 }
